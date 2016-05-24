156bf77a3db77de10f9411483d862d5a7f37d2d9
Lenovo <492321329@qq,com>
2016-05-24 09:21:51 +0800

    广告位

--- app/src/main/java/com/guozaiss/news/Constants.java
+++ app/src/main/java/com/guozaiss/news/Constants.java
@@ -7,4 +7,10 @@ public interface Constants {
     String AppKey = "cd5274d87dd4ef0540b77aca85a2c057";
     String news = "http://op.juhe.cn/onebox/news/query";
     String words = "http://op.juhe.cn/onebox/news/words";
+
+     String APP_ID = "100040112";
+     String SECRET_KEY = "d184542ace05b7cabe0d050aff74a143";
+     String BANNER = "d93ce8dfdf9f65b17cb568ab21798714";
+     String INTERSTITIAL = "2d1ccfaaab9a09d4be8eec7d86ccca77";
+     String APP_WALL = "66caff24c98802b40dbb014bbf39f0be";
 }
